241c5af7e69dd33e430c309ae350826d30e1583c
Alfin Syahrina <its-na@users.noreply.github.com>
2026-06-04 13:48:37 +0800

    fix: gunakan bypass TokenStorage untuk tembus sub-proses mlflow

--- modelling.py
+++ modelling.py
@@ -5,8 +5,22 @@ from sklearn.metrics import accuracy_score
 import mlflow
 import dagshub
 
+def setup_dagshub_auth():
+    # Mengambil token yang dikirim dari file YAML workflow
+    token = os.getenv("DAGSHUB_TOKEN_BYPASS")
+    if token:
+        print("🔧 Mengonfigurasi token otentikasi DagsHub secara manual...")
+        from dagshub.auth import TokenStorage
+        # Memaksa DagsHub menyimpan token di komputer virtual GitHub Runner
+        storage = TokenStorage()
+        storage.add_token_object("dagshub.com", {"token": token})
+        os.environ["DAGSHUB_CLIENT_TOKEN"] = token
+
 def train_model():
-    # Menginisialisasi koneksi ke DagsHub (Token dibaca otomatis dari sistem GitHub Actions)
+    # Jalankan konfigurasi bypass token sebelum inisialisasi
+    setup_dagshub_auth()
+    
+    # Inisialisasi koneksi ke DagsHub
     dagshub.init(repo_owner='pyrall0712', repo_name='Eksperimen_SML_MuhamadRizal', mlflow=True)
 
     # Mengambil data Iris hasil preprocessing dari repositori utama Anda
@@ -21,7 +35,6 @@ def train_model():
     X_test = test_df.drop(columns=['Species'])
     y_test = test_df['Species']
 
-    # Set nama eksperimen agar sama dengan di DagsHub
     mlflow.set_experiment("Iris_Classification_Baseline")
 
     with mlflow.start_run(run_name="CI_Automated_Run"):
@@ -34,13 +47,12 @@ def train_model():
         mlflow.log_param("n_estimators", 100)
         mlflow.log_metric("accuracy", acc)
         
-        # MENYIMPAN MODEL FISIK (Agar tab Artifacts di DagsHub tidak kosong)
         mlflow.sklearn.log_model(
             sk_model=model, 
             artifact_path="model",
             registered_model_name="Iris_RandomForest_Model"
         )
-        print(f"Berhasil! Akurasi Model CI: {acc:.4f}")
+        print(f"🚀 Berhasil! Akurasi Model CI: {acc:.4f}")
 
 if __name__ == "__main__":
     train_model()
